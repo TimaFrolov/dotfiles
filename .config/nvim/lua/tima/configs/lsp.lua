938e00977886309d9056d5867bdd49ac618adc4d
TimaFrolov <tima1911@yandex.ru>
2023-08-30 12:12:05 +0300

    Use telescope reference finder instaed of builtin

--- .config/nvim/lua/tima/configs/lsp.lua
+++ .config/nvim/lua/tima/configs/lsp.lua
@@ -1,27 +1,26 @@
 local lsp = require 'lsp-zero'
+local telescope = require("telescope.builtin")
 
 lsp.preset("recommended")
 
+local function on_attach(_, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    -- TODO: find a way to move it to mappings.lua, or make better file structure
+    vim.keymap.set('n', 'gr', telescope.lsp_references, { buffer = true })
+end
+
 local lspconfig = require 'lspconfig'
 
-lspconfig.lua_ls.setup({})
-lspconfig.clangd.setup({})
-lspconfig.rust_analyzer.setup({})
-lspconfig.tsserver.setup({
-  on_attach = function(client, _)
-    client.resolved_capabilities.document_formatting = false
-  end,
-})
-lspconfig.clangd.setup({})
-lspconfig.volar.setup({
-  on_attach = function(client, _)
-    client.resolved_capabilities.document_formatting = false
-  end,
-})
-lspconfig.cmake.setup({})
-lspconfig.pylsp.setup({})
-lspconfig.awk_ls.setup({})
-lspconfig.spectral.setup({})
-lspconfig.ocamllsp.setup({})
+lspconfig.lua_ls.setup({ on_attach = on_attach })
+lspconfig.clangd.setup({ on_attach = on_attach })
+lspconfig.rust_analyzer.setup({ on_attach = on_attach })
+lspconfig.tsserver.setup({ on_attach = on_attach })
+lspconfig.clangd.setup({ on_attach = on_attach })
+lspconfig.volar.setup({ on_attach = on_attach })
+lspconfig.cmake.setup({ on_attach = on_attach })
+lspconfig.pylsp.setup({ on_attach = on_attach })
+lspconfig.awk_ls.setup({ on_attach = on_attach })
+lspconfig.spectral.setup({ on_attach = on_attach })
+lspconfig.ocamllsp.setup({ on_attach = on_attach })
 
 lsp.setup()
