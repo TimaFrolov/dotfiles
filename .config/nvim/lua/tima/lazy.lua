c2f42123cc6a6ea239cfb50cf379bea3ec0e78ea
TimaFrolov <tima1911@yandex.ru>
2023-09-20 00:03:52 +0300

    Start lsp-zero and treesitter on start to update without InsertEnter

--- .config/nvim/lua/tima/lazy.lua
+++ .config/nvim/lua/tima/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = "InsertEnter",
+    event = "VeryLazy",
     config = function() require 'tima.configs.treesitter' end,
   },
   {
@@ -54,23 +54,23 @@ local plugins = {
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
-    event = "InsertEnter",
+    event = "VeryLazy",
     config = function() require 'tima.configs.lsp' end,
     dependencies = {
       -- LSP Support
-      { 'neovim/nvim-lspconfig' }, -- Required
-      {                            -- Optional
+      { 'neovim/nvim-lspconfig' },
+      {
         'williamboman/mason.nvim',
         build = function()
           pcall(vim.cmd, 'MasonUpdate')
         end,
       },
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },     -- Required
-      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'L3MON4D3/LuaSnip' },     -- Required
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
     },
   },
   {
