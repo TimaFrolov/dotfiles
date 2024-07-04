c636fc78b829edf762a483a75d705c96b335187b
TimaFrolov <tima1911@yandex.ru>
2024-07-04 09:33:02 +0300

    nvim: make copilot plugins opt-in using NVIM_GH_COPILOT environment variable

--- .config/nvim/lua/tima/plugins/copilot.lua
+++ .config/nvim/lua/tima/plugins/copilot.lua
@@ -4,6 +4,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function() require 'tima.configs.copilot' end,
     dependencies = { 'zbirenbaum/copilot.lua' },
+    enabled = function() return os.getenv('NVIM_GH_COPILOT') == 'true' end,
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
@@ -16,5 +17,6 @@ return {
       debug = true,
     },
     cmd = { "CopilotChat" },
+    enabled = function() return os.getenv('NVIM_GH_COPILOT') == 'true' end,
   },
 }
