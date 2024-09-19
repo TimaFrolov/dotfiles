1f0d9a4a82aaccc80a2b55b11a7c6bfdbc579524
TimaFrolov <tima1911@yandex.ru>
2024-09-19 12:08:33 +0300

    nvim(dap): Add eval expressions keybindings

--- .config/nvim/lua/tima/mappings/dapui.lua
+++ .config/nvim/lua/tima/mappings/dapui.lua
@@ -1,4 +1,5 @@
 return {
   { "<leader>do", function() require('dapui').toggle({ reset = true }) end, desc = "[D]ap [O]pen UI" },
   { "<leader>dk", function() require('dapui').eval() end,                   desc = "[D]ap show current symbol" },
+  { "<leader>dv", function() require('dapui').eval(vim.fn.input('Expression to eval:'), {enter=true}) end, desc = "[D]ap e[V]al"}
 }
