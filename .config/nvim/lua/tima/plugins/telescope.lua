52427de7d47c4434218688f548542c1a7d4dcc95
TimaFrolov <tima1911@yandex.ru>
2025-01-31 19:52:55 +0300

    nvim(telescope-fzf-native): update build command for newer versions

--- .config/nvim/lua/tima/plugins/telescope.lua
+++ .config/nvim/lua/tima/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
     {
       'nvim-telescope/telescope-fzf-native.nvim',
       build =
-      'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+      'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
     },
     'nvim-telescope/telescope-ui-select.nvim',
     'nvim-telescope/telescope-file-browser.nvim'
