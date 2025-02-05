c3c04b95e454473c04b58ccfcdf9ae06c943a17d
TimaFrolov <tima1911@yandex.ru>
2025-02-05 17:40:58 +0300

    nixos: add distrobox package

--- nixos/bundle/dev.nix
+++ nixos/bundle/dev.nix
@@ -1,7 +1,13 @@
 { config, lib, pkgs, ... }:
 {
   imports = [ ../package/neovim.nix ];
+
+  virtualisation.podman = {
+    enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     lazygit
+    distrobox
   ];
 }
