0a8b78ccdcdb01e0ab9464d3b70e651f641aa15b
TimaFrolov <tima1911@yandex.ru>
2025-05-03 14:23:38 +0300

    nixos: update pipewire config as recommended by wiki

--- nixos/bundle/desktop.nix
+++ nixos/bundle/desktop.nix
@@ -20,8 +20,11 @@
     openFirewall = true;
   };
 
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
   };
 
