61fdf78f4e910fc1ffd634966ef9c60cd063dec5
TimaFrolov <tima1911@yandex.ru>
2026-06-07 22:04:40 +0300

    nixos(desktop): switch to flake-based configuration

---
+++ nixos/config/desktop/flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations."desktop" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
