ee2f77356c9020d9d6af6aa9cbf92bf19239cf92
TimaFrolov <tima1911@yandex.ru>
2026-07-05 19:46:55 +0300

    fixup! hyprland: add configuration similar to sway one

--- nixos/config/desktop/home.nix
+++ nixos/config/desktop/home.nix
@@ -14,7 +14,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     plugins = with pkgs.hyprlandPlugins; [ hy3 ];
-    extraConfig = "require(tima)";
+    extraConfig = ''require("tima")'';
   };
 
   home.file.".config/hypr/tima" = {
