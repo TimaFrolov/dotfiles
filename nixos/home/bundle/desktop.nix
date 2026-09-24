0c3480ca00153ef3c24fb399337e06b56dc0e40e
TimaFrolov <tima1911@yandex.ru>
2026-09-24 22:24:31 +0300

    nixos: enable thunderbird

--- nixos/home/bundle/desktop.nix
+++ nixos/home/bundle/desktop.nix
@@ -1,4 +1,5 @@
 {
+  config,
   ...
 }:
 {
@@ -6,4 +7,11 @@
     ../package/hyprland.nix
     ../package/waybar.nix
   ];
+
+  programs.thunderbird = {
+    enable = true;
+    profiles.${config.home.username}.isDefault = true;
+  };
+
+  catppuccin.thunderbird.profile = config.home.username;
 }
